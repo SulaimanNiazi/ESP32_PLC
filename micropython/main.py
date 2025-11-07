3923d830222b373353544f0aea6843f0c2a8123b
Muhammad Sulaiman <muhammad.sulaiman@emumba.com>
2025-11-07 13:27:03 +0500

    Develop code for changing any pin to input, or output (low or high)

--- micropython/main.py
+++ micropython/main.py
@@ -1,3 +1,31 @@
-while True:
-    line = input("Enter: ").lower().strip()
-    print(line)
+from machine import Pin
+
+def main():
+    while True:
+        line = input('> ').lower().strip()
+        if line[0].isdigit():
+            params = line.split(' ')
+            
+            if (len(params) == 3) and (params[1]=='='):
+                try:
+                    pin = int(params[0])
+                    state = params[2]=='x'
+
+                    p = Pin(pin, Pin.IN if state else Pin.OUT)
+                    if not state:
+                        if params[2]=='1':
+                            p.on()
+                        elif params[2]=='0':
+                            p.off()
+                        else:
+                            print('?')
+                except:
+                    print('?')
+            else:
+                print('?')
+        else:
+            print('?')
+
+if __name__ == '__main__':
+    print()
+    main()
